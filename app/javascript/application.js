347295cca636828b44de50272c6f981478f91c68
shadmanAh <shadman061@gmail.com>
2022-01-27 11:34:16 +0330

    create post

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery
 //= require toastr
+//= require dropzone
 
 import "@hotwired/turbo-rails"
 import "controllers"
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :post
 
+  validates :image, presence: true
+  
+  
 
   mount_uploader :image, PhotoUploader
 end
