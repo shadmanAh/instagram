dd8fbea014464fc8e96b9d6041ad543bcb3bceca
shadmanAh <shadman061@gmail.com>
2022-01-29 14:04:48 +0330

    add comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
   def show 
     @photos = @post.photos
     @likes = @post.likes.includes(:user)
+    @comment = Comment.new
     @is_liked = @post.is_liked(current_user)
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_080649) do
+ActiveRecord::Schema.define(version: 2022_01_29_091707) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -55,6 +65,8 @@ ActiveRecord::Schema.define(version: 2022_01_28_080649) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "photos", "posts"
