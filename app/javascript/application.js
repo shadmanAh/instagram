8c7645c739789df4eaf484a00895bfbc5bdfcd1e
shadmanAh <shadman061@gmail.com>
2022-01-29 10:14:56 +0330

    add likes_controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery
+//= require jquery_ujs
 //= require toastr
 //= require dropzone
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_120410) do
+ActiveRecord::Schema.define(version: 2022_01_28_080649) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "image"
@@ -46,6 +55,8 @@ ActiveRecord::Schema.define(version: 2022_01_26_120410) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
 end
