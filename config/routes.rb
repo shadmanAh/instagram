2e6b818eab93b11858dfaf0770532772ee02acc8
shadmanAh <shadman061@gmail.com>
2022-01-29 12:08:51 +0330

    like and dislike

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :create, :destroy] do 
     resources :photos, only: [:create]
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy], shallow: true
   end
 end
+
+# shallow create the shorter Address                                                     <======
